38b7da0d96b7a3f4d3be9d8093e69e90e83337de
Jerimiah Milton <jerimiah.milton@gmail.com>
2015-12-10 14:06:00 -0600

    Updated the field names in the JSON data to be more consistent.

--- api/app/models/epa_record.rb
+++ api/app/models/epa_record.rb
@@ -17,9 +17,9 @@ class EpaRecord < ActiveRecord::Base
   }
 
   scope :county_totals, -> {
-    subquery = select('facility_state, facility_county, COUNT(*), SUM(CASE WHEN total_air_emissions~E\'^\\\\d+$\' THEN total_air_emissions::float ELSE 0 END) AS total_air, SUM(CASE WHEN total_underground_injection~E\'^\\\\d+$\' THEN total_underground_injection::float ELSE 0 END) AS total_underground, SUM(CASE WHEN total_on_site_land_releases~E\'^\\\\d+$\' THEN total_on_site_land_releases::float ELSE 0 END) AS total_land, SUM(CASE WHEN total_surface_water_discharge~E\'^\\\\d+$\' THEN total_surface_water_discharge::float ELSE 0 END) AS total_water').group('facility_state, facility_county')
-    select('facility_state, facility_county, count, total_air, total_underground, total_land, total_water, total_air + total_underground + total_land + total_water AS total').from("(#{subquery.to_sql}) AS t1").map do |x|
-      {state: x.facility_state, county: x.facility_county, count: x.count, total_air: x.total_air, total_underground: x.total_underground, total_land: x.total_land, total_water: x.total_water, total: x.total}
+    subquery = select('facility_state, facility_county, COUNT(*), SUM(CASE WHEN total_air_emissions~E\'^\\\\d+$\' THEN total_air_emissions::float ELSE 0 END) AS total_air_emissions, SUM(CASE WHEN total_underground_injection~E\'^\\\\d+$\' THEN total_underground_injection::float ELSE 0 END) AS total_underground_injection, SUM(CASE WHEN total_on_site_land_releases~E\'^\\\\d+$\' THEN total_on_site_land_releases::float ELSE 0 END) AS total_on_site_land_releases, SUM(CASE WHEN total_surface_water_discharge~E\'^\\\\d+$\' THEN total_surface_water_discharge::float ELSE 0 END) AS total_surface_water_discharge').group('facility_state, facility_county')
+    select('facility_state, facility_county, count, total_air_emissions, total_underground_injection, total_on_site_land_releases, total_surface_water_discharge, total_air_emissions + total_underground_injection + total_on_site_land_releases + total_surface_water_discharge AS total').from("(#{subquery.to_sql}) AS t1").map do |x|
+      {state: x.facility_state, county: x.facility_county, count: x.count, total_air_emissions: x.total_air_emissions, total_underground_injection: x.total_underground_injection, total_on_site_land_releases: x.total_on_site_land_releases, total_surface_water_discharge: x.total_surface_water_discharge, total: x.total}
     end
   }
 end
