20e049962651943bb806034b365917ccd59de3e8
voxlol <voxlol@outlook.com>
2015-12-14 15:56:55 -0600

    Grab filter form values on submit

--- www/app/features/main/main.js
+++ www/app/features/main/main.js
@@ -11,5 +11,21 @@ angular.module('gapFront')
   .controller('MainCtrl', MainController);
 
 function MainController($scope) {
-  
+  $scope.submitMapFilters = function(e) {
+    var active = $('#emission').find('.active').map(function(){
+      return this.textContent.trim();
+    }).get();
+
+    if(active.indexOf('Total') >= 0 || ['Land', 'Water', 'Air'].every(allOptionsChosen)){
+      active = ['Total'];
+    }
+
+    
+
+    // Private
+
+    function allOptionsChosen(option) {
+      return active.indexOf(option) >= 0;
+    }
+  }  
 }
