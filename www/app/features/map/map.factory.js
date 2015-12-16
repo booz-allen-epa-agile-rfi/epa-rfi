aee8407fc0f10d57401bf6c507f78faffdaab08e
voxlol <voxlol@outlook.com>
2015-12-16 14:04:43 -0600

    Clean up map factory and remove load of counties.js

--- www/app/features/map/map.factory.js
+++ www/app/features/map/map.factory.js
@@ -14,7 +14,6 @@
 
     Map.update = update;
     Map.map = initMap();
-    addToMap(Map.map);
 
     Map.loaded = true;
 
@@ -48,12 +47,6 @@
 
         Map.dataLayers.geojson.addTo(Map.map);
 
-        // if(queryParams.bounds && queryParams.bounds.length === 4) {
-        //   var sw = queryParams.bounds.slice(0,2);
-        //   var ne = queryParams.bounds.slice(2,4);
-        //   Map.map.fitBounds([sw,ne]);
-        // }
-
         function formatData(results) {
           var health_effects = 'cercla_chemicals haps priority_chemicals osha_chemicals body_weight cardiovascular dermal developmental endocrine gastrointestinal hematological hepatic immunological metabolic musculoskeletal neurological ocular other_systemic renal reproductive'.split(' ');
 
@@ -82,21 +75,6 @@
           }
         }
       });
-    }
-
-    function addToMap(mapInstance) {
-      var baseMaps = {
-        // 'Grayscale': Map.tileLayers.grayScale,
-        'Streets': Map.tileLayers.streets
-      }
-
-      // var overlayMaps = {
-      //   'Counties': Map.dataLayers.county,
-      //   'Air Quality': Map.dataLayers.air
-      // }
-
-      // L.control.layers(baseMaps, overlayMaps).addTo(mapInstance);
-      // Map.dataLayers.county.addTo(mapInstance);
     }
 
     function initMapTiles() {
@@ -129,131 +107,6 @@
         minZoom: 8,
         layers: [Map.tileLayers.streets]      // Renders the stuff
       });
-    }
-
-    function initData() {
-      // prepare the data
-      // prepareData(mockCountiesData, );
-
-      // var countyLayer = initCountyLayer();
-      // var airLayer = initAirLayer();
-
-      // return {
-      //   county: countyLayer,
-      //   air: airLayer
-      // }
-
-      // Private
-
-      // function prepareData(mockCounty, ) {
-      //   apiData.forEach(function(apiRow){
-      //     mockCounty.features.forEach(function(county){
-      //       if(county.properties.STATE === apiRow.state && county.properties.NAME.toUpperCase() === apiRow.county){
-      //         county.properties.total_air_emissions = apiRow.total_air_emissions;
-      //         county.properties.total_on_site_land_releases = apiRow.total_on_site_land_releases;
-      //         county.properties.total_underground_injection = apiRow.total_underground_injection;
-      //         county.properties.total_surface_water_discharge = apiRow.total_surface_water_discharge;
-      //         county.properties.total = apiRow.total;
-      //       }
-      //     });
-      //   });
-      // }
-
-      function initAirLayer() {
-        return omnivore.csv('features/map/mockData/data1r_randomized2.csv')           // Air Quality Data
-          .on('ready', function(layer) {
-            this.eachLayer(function(marker){
-              marker.setIcon(new L.Icon({       // sets the marker icon
-                iconUrl: 'assets/Pin_1.png',
-                iconSize: [20, 25]
-              }));
-
-              marker.bindPopup("<b>"+ marker.feature.properties['FACILITY.NAME'] +"</b><br />" + marker.feature.properties['FACILITY.CITY']);
-
-              marker.on('click', function(e){
-                console.log('you clicked', marker)
-                marker.openPopup();
-              })
-            });
-          });
-      }
-
-      function initCountyLayer(apiData) {
-        return L.geoJson(mockCountiesData, {
-          style: style,
-          onEachFeature: function(feature, layer){
-            createPop();
-            layer.on({
-              // click: zoomToFeature,
-              mouseover: highlightFeature,
-              mouseout: resetHighlight
-            });
-
-            // debuggging
-            layer.on('click', function(e) {
-              console.log(e.target.feature.properties);
-            });
-
-            // Private
-
-            function createPop() {
-              var countyPopUp = '<b>County Name : ' + feature.properties['NAME'] + "</b><br/>" +
-                          'State : ' + feature.properties['STATE'] + '<br />' +
-                          'Air Emissions : ' + feature.properties['total_air_emissions'] + '<br />' +
-                          'On Site Land Releases : ' + feature.properties['total_on_site_land_releases'] + '<br />' +
-                          'Underground Injection : ' + feature.properties['total_underground_injection'] + '<br />' +
-                          'Surface Water Discharge : ' + feature.properties['total_surface_water_discharge']
-
-              layer.bindPopup(countyPopUp);
-            }
-
-            function zoomToFeature(e){
-              Map.map.fitBounds(e.target.getBounds());
-            }
-
-            function highlightFeature(e){
-              var layer = e.target;
-
-              layer.setStyle({
-                  weight: 5,
-                  color: '#000',
-                  dashArray: '',
-                  fillOpacity: 0.7
-              });
-
-              if (!L.Browser.ie && !L.Browser.opera) {
-                  layer.bringToFront();
-              }
-            }
-
-            function resetHighlight(e) {
-              countyLayer.resetStyle(e.target);
-            }
-          }
-        });
-
-        function getColor(d) {
-            return d > 10000000 ? '#800026' :
-                   d > 490000  ? '#BD0026' :
-                   d > 350000 ? '#E31A1C' :
-                   d > 225000  ? '#FC4E2A' :
-                   d > 55000  ? '#FD8D3C' :
-                   d > 20000   ? '#FEB24C' :
-                   // d > 100   ? '#FED976' :
-                              '#FED976';
-        }
-
-        function style(feature) {
-            return {
-                fillColor: getColor(feature.properties.total),
-                weight: 2,
-                opacity: 1,
-                color: 'white',
-                dashArray: '3',
-                fillOpacity: 0.7
-            };
-        }
-      }
     }
   }
 })();
