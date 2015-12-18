5ae436750ab8404274330b1703fb2b10ac9398f8
Sonja Hall <hall_sonja@bah.com>
2015-12-18 09:24:09 -0600

    added a couple more tests

--- www/test/spec/controllers/main.js
+++ www/test/spec/controllers/main.js
@@ -21,63 +21,33 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  // TEST #1: Search bar empty on page load
-
-  it('should have nothing in search area on page load', function () {
+  it('should have clear search area on page load', function () {
     expect(scope.searchValue).toBe('');
   });
 
   describe('scope.state', function(){
-    it('should be an array of strings', function() {
+    it('should be an array of strings for emissions', function() {
       _.each(scope.emissions, function(emission) {
         expect(typeof emission).toEqual('string');
       });
     });
+    it('should be an array of strings for reporting_year', function() {
+      _.each(scope.reporting_year, function(reporting_year) {
+        expect(typeof reporting_year).toEqual('string');
+      });
+    });
+    it('should be an array of strings for bounds', function() {
+      _.each(scope.bounds, function(bounds) {
+        expect(typeof bounds).toEqual('string');
+      });
+    });
   });
 
-  //it('should add to the list of adverse events if a drug searched', function(){
-  //    scope.selectedDrug = 'advil';
-  //    setTimeout(function() {
-  //      expect(scope.searchDrugEvents()).toBeTruthy();
-  //    }, 5000);
-  //});
-  //
-  //it('should set totalLabels to function param when called', function(){
-  //  expect(scope.totalLabels).toBeUndefined();
-  //  var resp = {
-  //    meta: {
-  //      results: {
-  //        total: 5
-  //      }
-  //    }
-  //  };
-  //  scope.setTotalLabels(resp);
-  //  expect(scope.totalLabels).toBe(5);
-  //});
-  //
-  //it('should set totalEvents to function param when called', function(){
-  //  expect(scope.totalEvents).toBeUndefined();
-  //  var resp = {
-  //    meta: {
-  //      results: {
-  //        total: 5
-  //      }
-  //    }
-  //  };
-  //  scope.setTotalEvents(resp);
-  //  expect(scope.totalEvents).toBe(5);
-  //});
-  //
-  //it('should set selected drug when setSelectedDrug is called', function(){
-  //  expect(scope.selectedDrug).toBeUndefined();
-  //  scope.setSelectedDrug('advil');
-  //  expect(scope.selectedDrug).toBeDefined();
-  //});
-
-  //it('should set text to search correctly when the first or second drug input box is used', function(){
-  //  var firstBox = $('#searchTextResultView').val('advil');
-  //  console.log(scope.drugs);
-  //  scope.searchDrugs();
-  //  console.log(scope.drugs);
-  //});
+  it('should contain bounds, emissions and reporting_year in the generateShareUrl', function(){
+    expect(scope.generateShareUrl).toBeUndefined();
+    var url = 'http://treeview.io:9000' + '/' +
+      '?bounds=' +
+      '&emissions=' +
+      '&reporting_year='
+  });
 });
