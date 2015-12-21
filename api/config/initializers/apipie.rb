32af9c70daa1429f79e2947e6cd100b5c73402c4
Jerimiah Milton <jerimiah.milton@gmail.com>
2015-12-21 15:06:29 -0600

    API documentation changes and removed non-health effects. closes #324

--- api/config/initializers/apipie.rb
+++ api/config/initializers/apipie.rb
@@ -1,7 +1,7 @@
 Apipie.configure do |config|
   config.app_name                = 'EpaRfi'
-  config.api_base_url            = ':3000'
-  config.doc_base_url            = '/apipie'
+  config.api_base_url            = ''
+  config.doc_base_url            = '/documentation'
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/*.rb"
 end
